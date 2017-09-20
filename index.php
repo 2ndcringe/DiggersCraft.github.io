a22917ccf3f924c6053af06df9817fc64449b844
TaratorkinYT <TaratorkinYT@users.noreply.github.com>
2017-09-20 22:26:44 +0300

    nhj
    ui

---
+++ index.php
@@ -0,0 +1,228 @@
+<html>
+  
+  <head>
+    <meta charset="utf-8">
+    <title>DEV.RAVAGEW.RU - Minecraft сервера</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="http://bootswatch.com/yeti/bootstrap.min.css"
+    rel="stylesheet">
+    <script type="text/javascript" src="/js/jquery.min.js"></script>
+    <script type="text/javascript" src="/js/bootstrap.min.js"></script>
+  </head>
+  
+  <body style="background: url('/img/bg.jpg') no-repeat fixed 50% 0 rgb(20,20,20);">
+    <div class="navbar navbar-btne navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <a type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="glyphicon glyphicon-th-large"></span></a>
+          <span class="sr-only">Навигация</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <a class="navbar-brand" href="/">DEV.RAVAGEW.RU</a>
+        </div>
+        <div class="collapse navbar-collapse navbar-ex1-collapse">
+          <ul class="nav navbar-nav navbar">
+            <li>
+              <a href="#">Сервера</a>
+            </li>
+            <li>
+              <a href="#">Личный кабинет</a>
+            </li>
+            <li>
+              <a href="#">Магазин блоков</a>
+            </li>
+            <li>
+              <a href="#">Правила</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12"></div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="panel panel-primary">
+            <div class="panel-body">
+              <div id="carousel-example-generic" data-interval="false" class="carousel slide"
+              data-ride="carousel">
+                <div class="carousel-inner">
+                  <div class="item active">
+                    <img src="http://placehold.it/1920x600">
+                    <div class="carousel-caption">
+                      <h2>У нас уже играют {player_size}, а ты?</h2>
+                      <p>
+                        <a href="#" class="btn-lg btn btn-danger dropdown-toggle">Начать игру</a>
+                      </p>
+                    </div>
+                  </div>
+                  <div class="item">
+                    <img src="http://placehold.it/1920x600">
+                    <div class="carousel-caption">
+                      <h2>Последний игрок {player_last}, а ты?</h2>
+                      <p>
+                        <a href="#" class="btn-lg btn btn-danger dropdown-toggle">Начать игру</a>
+                      </p>
+                    </div>
+                  </div>
+                  <div class="item">
+                    <img src="http://placehold.it/1920x600">
+                    <div class="carousel-caption">
+                      <h2>Нажимай и играй!</h2>
+                      <p>
+                        <a href="#" class="btn-lg btn btn-danger dropdown-toggle">Начать игру</a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+                <a class="right carousel-control" href="#carousel-example-generic" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-8">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">Декабрьский вайп (завершён). Что нас ждёт</h3>
+            </div>
+            <div class="panel-body">
+              <p>Всем доброго времени суток, дорогие игроки. Согласно нашей традиции, мы
+                уже Вас оповестили о предстоящем вайпе за месяц до него. С каждым днём
+                до него остаётся ждать всё меньше и меньше и вопрос "когда?" становится
+                наиболее актуальным. Пришло время ответить на вопросы и рассказать о будущих</p>
+            </div>
+            <div class="panel-footer">
+              <span class="glyphicon glyphicon-calendar"></span>&nbsp;22.12.15
+              <a href="#" class="btn btn-info btn-xs pull-right">Подробнее</a>
+              <span class="glyphicon glyphicon-user"></span>&nbsp;Corndan</div>
+          </div>
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">Открытие сервера HardTech</h3>
+            </div>
+            <div class="panel-body">
+              <p>Уже много раз игроки HiTech серверов нас просили сделать сложный технический
+                сервер с GregTech и сегодня пришло время исполнить их желание. Бета-тестирование
+                завершено, спаун построен, теперь сервер готов принимать игроков.</p>
+            </div>
+            <div class="panel-footer">
+              <span class="glyphicon glyphicon-calendar"></span>&nbsp;22.12.15
+              <a href="#" class="btn btn-info btn-xs pull-right">Подробнее</a>
+              <span class="glyphicon glyphicon-user"></span>&nbsp;Corndan</div>
+          </div>
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">С днём знаний!</h3>
+            </div>
+            <div class="panel-body">
+              <p>Всё хорошее рано или поздно кончается. Лето подошло к концу. Мы с вами
+                за это время добились многого и будем это продолжать это делать. Администрация
+                RedServer поздравляет всех учащихся с днём знаний и желает Вам успехов
+                в учёбе.</p>
+            </div>
+            <div class="panel-footer">
+              <span class="glyphicon glyphicon-calendar"></span>&nbsp;22.12.15
+              <a href="#" class="btn btn-info btn-xs pull-right">Подробнее</a>
+              <span class="glyphicon glyphicon-user"></span>&nbsp;Corndan</div>
+          </div>
+          <ul class="pagination">
+            <li>
+              <a href="#">Назад</a>
+            </li>
+            <li>
+              <a href="#">1</a>
+            </li>
+            <li>
+              <a href="#">2</a>
+            </li>
+            <li>
+              <a href="#">Вперед</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-md-4">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">Авторизация</h3>
+            </div>
+            <div class="panel-body">
+              <div class="input-group input-group-sm">
+                <span class="input-group-addon">
+                  <span class="glyphicon glyphicon-user"></span>
+                </span>
+                <input type="text" name="login_name" class="form-control" placeholder="Логин:">
+                <span class="input-group-btn">
+                  <a href="/index.php?do=register" class="btn btn-default">Регистрация</a>
+                </span>
+              </div>
+              <br>
+              <div class="input-group input-group-sm">
+                <span class="input-group-addon">
+                  <span class="glyphicon glyphicon-lock"></span>
+                </span>
+                <input type="password" name="login_password" class="form-control" placeholder="Пароль:">
+                <span class="input-group-btn">
+                  <a href="http://hi-tegra.ru/index.php?do=lostpassword" class="btn btn-default">Забыли ?</a>
+                </span>
+              </div>
+              <br>
+              <button class="btn btn-success top10 btn-block" type="submit" onclick="submit();">Войти</button>
+            </div>
+          </div>
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">Профиль</h3>
+            </div>
+			   <div class="panel-body">
+              <img src="/skin.png" class="pull-left top5 imgCom img-rounded img-responsive" width="90">&nbsp;&nbsp;&nbsp;&nbsp;
+              <a href="">Профиль</a>
+              <br>&nbsp;&nbsp;&nbsp;&nbsp;
+              <a href="">Лк</a>
+              <br>&nbsp;&nbsp;&nbsp;&nbsp;
+              <a href="">Магазин Блоков</a>
+              <br>&nbsp;&nbsp;&nbsp;&nbsp;
+              <a href="">Кнопка</a>
+              <br>
+              <br>
+              <button class="btn btn-success top10 btn-block" type="submit" onclick="submit();">Выйти</button>
+          </div></div>
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">Мониторинг</h3>
+            </div>
+            <center>
+<div class="panel-body">
+<div style="float: left;">Rusty PE</div><div style="float: right;">18/40</div><br><div class="progress progress-striped active">
+<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 45%"></div></div><div style="float: left;">Hi-Tech PC</div><div style="float: right;">Оффлайн</div><br><div class="progress progress-striped active">
+<div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div></div> </div>
+</center>
+          </div>
+        </div>
+      </div>
+      <div class="panel panel-primary">
+        <div class="panel-heading">
+          <h3 class="panel-title">
+            <center>DEV.RAVAGEW.RU 2014 ©</center>
+          </h3>
+        </div>
+      </div>
+    </div>
+    <br>
+    <br>
+  </body>
+
+</html>
